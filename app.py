e8c757f5395bdea776e414bd73482e7890d006de
nguyenmyngoc9953-source <nguyenmyngoc9953@gmail.com>
2026-06-18 14:49:16 +0700

    Update app.py

--- app.py
+++ app.py
@@ -18,6 +18,6 @@ else:
 print("Thu nhập chịu thuế:", taxable_income)
 print("Thuế TNCN phải nộp:", tax)
 print("Lương thực nhận:", salary - tax)
-print("Gross Income:", sample_gross)
+print("Gross Income:", salary)
 print(f"Dependents : {sample_dependents}")
 print(f"PIT Owed     : {tax_owed:,.0f} VND")
